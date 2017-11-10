9b41b21a028f949a7f14cb4ded08b1b684cfc0d4
Nityams <nityamshrestha@gmail.com>
2017-11-10 13:41:12 -0800

    Fragment orientation
    DONE

--- AlphaFitness/app/src/main/java/nityam/com/alphafitness/MainActivity.java
+++ AlphaFitness/app/src/main/java/nityam/com/alphafitness/MainActivity.java
@@ -1,5 +1,8 @@
 package nityam.com.alphafitness;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Configuration config = getResources().getConfiguration();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+
+        if (config.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            RecordWorkout recordWorkout = new RecordWorkout();
+            fragmentTransaction.replace(android.R.id.content,recordWorkout);
+        } else {
+            WorkoutDetails workoutDetails = new WorkoutDetails();
+            fragmentTransaction.replace(android.R.id.content, workoutDetails);
+        }
+        fragmentTransaction.commit();
     }
 }
