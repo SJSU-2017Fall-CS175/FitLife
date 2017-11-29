c49ddad0574d3513a62ec080b29c3156a9ecad68
Nityams <nityamshrestha@gmail.com>
2017-11-29 12:22:39 -0800

    with getLocation() = null

--- app/src/main/java/nityam/com/fitlife/HomePage.java
+++ app/src/main/java/nityam/com/fitlife/HomePage.java
@@ -1,17 +1,24 @@
 package nityam.com.fitlife;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +36,8 @@ import java.text.DecimalFormat;
 import java.util.Random;
 
 public class HomePage extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
+    private final int REQUEST_CODE = 0;
+
     private GoogleMap mMap;
     private boolean isWorkingOut = false;
     private Button workout;
@@ -50,6 +59,7 @@ public class HomePage extends FragmentActivity implements OnMapReadyCallback, Se
     Handler handler;
     DecimalFormat df;
 
+    private boolean isFirstLaunch = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -109,11 +119,56 @@ public class HomePage extends FragmentActivity implements OnMapReadyCallback, Se
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if ((ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) ||
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            mMap.setMyLocationEnabled(true);
+
+        }
+
+        LocationManager locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        Location l = locManager != null ? locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER) : null;
+
+        if (l == null) {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            String provider = locManager != null ? locManager.getBestProvider(criteria, true) : null;
+            assert locManager != null;
+            l = locManager.getLastKnownLocation(provider);
+
+            if (isFirstLaunch) {
+                isFirstLaunch = false;
+                googleMap.addMarker(new MarkerOptions().position(
+                        new LatLng(l.getLatitude(), l.getLongitude())).title("Current location"));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(l.getLatitude(), l.getLongitude()), 16));
+            } else {
+                isFirstLaunch = false;
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(
+                        new LatLng(l.getLatitude(), l.getLongitude())));
+            }
+
+        } else {
+
+            if (isFirstLaunch) {
+                isFirstLaunch = false;
+                googleMap.addMarker(new MarkerOptions().position(
+                        new LatLng(l.getLatitude(), l.getLongitude())).title("Current location"));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(l.getLatitude(), l.getLongitude()), 16));
+            } else {
+                isFirstLaunch = false;
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(
+                        new LatLng(l.getLatitude(), l.getLongitude())));
+            }
+        }
+
     }
 
     public void uerProfileClicked(View view) {
